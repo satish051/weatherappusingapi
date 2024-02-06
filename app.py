076052c01d24c67ba608a138b4f061f3ecd5e9c6
satish chandra karki <karkisatishit@kbc.edu.np>
2024-02-06 19:48:41 +0545

    not done yet

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+# OpenWeatherMap API key (replace with your own)
+API_KEY = "your_openweathermap_api_key"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/weather', methods=['POST'])
+def weather():
+    city = request.form['city']
+    if not city:
+        return render_template('index.html', error='Please enter a city name.')
+
+    # Fetch weather data from OpenWeatherMap API
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        weather_data = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+        }
+        return render_template('weather.html', weather_data=weather_data)
+    else:
+        error_message = data['message'] if 'message' in data else 'Unknown error'
+        return render_template('index.html', error=f'Error: {error_message}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
